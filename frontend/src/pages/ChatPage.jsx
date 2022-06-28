4053292c6bdc7e4185c304231b0bc98a01d76250
birendra20 <birendragupta20@gmail.com>
2022-06-28 19:09:32 +0530

    api fetch

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  const [chats, setChats] = useState([]);
+
+  const fetchChat = async () => {
+    const { data } = await axios.get("/api/chat");
+    setChats(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchChat();
+  }, []);
+
+  return (
+    <div>
+      {chats.map((chat) => {
+        return <div key={chat._id}>{chat.chatName}</div>;
+      })}
+      hello
+    </div>
+  );
 };
 
 export default ChatPage;
